bc9f9557ab8f31a87555c5e63af2a7a0a8eaa0fe
Prajwal r <prajwal@prajwal>
2022-12-20 19:22:18 +0530

    feat: test coverage 2

---
+++ src/pages/__tests__/index.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import Home from "..";
+
+it("renders without crashing", () => {
+  render(<Home />);
+});
--- src/pages/__tests__/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM, { render } from 'react-dom';
-import Home from '..';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<Home />, div);
-});
